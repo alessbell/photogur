3cf5a60d09fa3e6f2ddbd3e987786f4cd20b26e6
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-06-17 15:40:12 -0400

    Refactoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,31 +5,25 @@ class PicturesController < ApplicationController
 	end
 
 	def show
-		@pictures = Picture.find(params[:id])
-		# 	{
-		# 		:title => "The old church on the coast of White sea",
-		# 		:artist => "Sergey Ershov",
-		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-		# 	},
-		# 	{
-		# 		:title => "Sea Power",
-		# 		:artist => "Stephen Scullion",
-		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-		# 	},
-		# 	{
-		# 		:title => "Into the Poppies",
-		# 		:artist => "John Wilhelm",
-		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-		# 	}
-		# ]
-		# @picture = @pictures[params[:id].to_i]
+		@picture = Picture.find(params[:id])
 	end
 
 	def new
+		@picture = Picture.new
 	end
 
 	def create
-		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
 	end
 
 end
